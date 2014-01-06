491f59fc8d46a420ea3f4dd859701e67cba6ecf8
sebdi <s.dingler@googlemail.com>
2014-01-06 04:28:54 +0100

    Now only stable methods in master branch. Unstable methods now in beta branch

--- KalmanFilter.m
+++ KalmanFilter.m
@@ -1,5 +1,5 @@
 classdef KalmanFilter<handle
-
+    
     properties (SetAccess = public)
         T;      % sample period
         F;      % system matrix
@@ -28,20 +28,15 @@ classdef KalmanFilter<handle
             obj.Q = inModel.Q;
         end
         
-        % INITIALIZATION
+        %% INITIALIZATION
         function initInitialState(obj, inX) % start value of state for recursion
             obj.x=inX;
         end
         function initInitialCovariance(obj, inP) % start value of covariance for recursion
             obj.P=inP;
         end
-        %%
-        function [ret]=reset(obj)
-            obj.P=P_init;
-            ret=1;
-        end
         
-        % RUN
+        %% RUN
         function obj = run(obj, measurements, u)
             if obj.checkValues()
                 obj.state{1}=obj.x;
@@ -79,49 +74,5 @@ classdef KalmanFilter<handle
                 obj.x
             end
         end
-        %% getState(line)
-        %  For line=i with i=1, ... return value is state element at position i
-        %  For line<1 return value is state vector as an array
-        function [x_line]=getState(obj,line)
-            if line>0
-                x_line=nan(length(obj.state),1);
-                for i=1:length(obj.state)
-                    x_line(i)=obj.state{i}(line);
-                end
-            else
-                x_line=nan(size(obj.state{1},1),length(obj.state));
-                for i=1:length(obj.state)
-                    x_line(:,i)=obj.state{i};
-                end
-            end
-        end
-        %% getCovariance(line,column)
-        function [P_ret]=getCovariance(obj, line, column)
-            if line>0 && column>0
-                P_ret=nan(1,length(obj.state));
-                for i=1:length(obj.state)
-                    P_ret(i)=obj.covariance{i}(line,column);
-                end
-            else
-                for i=1:length(obj.state)
-                    P_ret{i}=obj.covariance{i};
-                end
-            end
-        end
-    end
-    methods (Access=private)
-        function [x,P]=predict(obj)
-            obj.x=obj.F*obj.x+obj.G*u(i);
-            obj.P=obj.F*obj.P*obj.F'+obj.Q;
-            z=obj.H*obj.x;
-            % Filering
-            %obj.S=obj.R+obj.H*obj.P*obj.H';
-            %obj.K=obj.P*obj.H'*pinv(obj.S);
-            %obj.x=obj.x+obj.K*(measurements(i)-z);
-            %obj.P=obj.P-obj.K*obj.S*obj.K';
-            % Joseph Form
-            %obj.P=(eye(size(obj.P,2))-obj.K*obj.H)*obj.P*(eye(size(obj.P,2))-obj.K*obj.H)'+obj.K*obj.R*obj.K';
-        end
     end
 end
-
